58f212c345ae64ec269d10f996a702d2f907dfc5
herboren <herboren@users.noreply.github.com>
2021-07-04 16:24:10 -0400

    Update valenc.py

--- valenc.py
+++ valenc.py
@@ -1,3 +1,27 @@
 # In chemsitry the valency of an and element is the
 # measure of the combining capacity with other atoms,
-import binascii, os, hashlib
+from requests import Request, Session, exceptions
+from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
+import json
+
+url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
+parameters = {
+    'start':'1',
+    'limit':'10',
+    'convert':'USD'
+}
+
+header = {
+    'Accepts':'application/json',
+    'X-CMC_PRO_API_KEY':'${{ secrets.VAL_CMC_API }}'
+}
+
+session = Session()
+session.headers.update(header)
+
+try:
+    response = session.get(url, params=parameters)
+    data = json.loads(response.text)
+    print(data)
+except (ConnectionError, Timeout, TooManyRedirects) as ex:
+    print(ex)
